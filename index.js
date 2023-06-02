13c846b6f440384c18d0f5fa940de556c054a058
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-06-02 15:51:12 +0100

    Add react projects

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
 // data for all cards
 const data = [
   {
-    id: 1,
-    detailUrl: '#_react',
-    liveUrl: 'https://musong-to-do.netlify.app/',
-    sourseUrl: 'https://github.com/ernestmusong/To-do-List',
-    img: 'images/works-images/front2.png',
-    name: 'To Do List',
-    desc: `It's an application that allows users to add, edit and remove items from the list. All data is saved to local storage`,
-    technologies: ['javascript', 'html5', 'css', 'webpack'],
-    skills: ['javascript', 'css', 'html5'],
-    skillsImg: 'images/icon-languages.png',
-    desktopPopUpClose: 'images/desktop-popup-close.png',
-    mobilePopUpClose: 'images/close-detail.png',
-    seeLiveIcon: 'images/see-live.png',
-    seeSourceIcon: 'images/see-source.png',
-  },
-  {
     id: 2,
-    detailUrl: '#_js',
-    liveUrl: '/contribute-page',
-    sourseUrl: '/contribute-page',
-    img: 'images/works-images/html-css.jpg',
-    name: 'html project',
-    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Ipsum has been the industry's standard dummy text
-    ever since the 1500s, when an unknown printer took a galley of
-    type and scrambled it 1960s with the releaLorem Ipsum is simply
-    dummy text of the printing and typesetting industry. Lorem Ipsum
-    has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it 1960s 
-    with the relea`,
-    technologies: ['javascript', 'ruby on rails', 'css'],
+    detailUrl: '#_react',
+    liveUrl: 'https://musong-furniture-store.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/React-House-Furniture-Store',
+    img: 'images/works-images/furniture-store.png',
+    name: 'furniture store',
+    desc: `An E-commerce Web App that enables users to buy online with the
+    following features: Add cart items, Remove cart items, Clear cart,
+    Calculate tax, Sub totals, and totals.`,
+    technologies: ['React', 'Redux', 'Bootstrap'],
     skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
     skillsImg: 'images/icon-languages.png',
     desktopPopUpClose: 'images/desktop-popup-close.png',
@@ -43,18 +22,13 @@ const data = [
     id: 3,
     detailUrl: '#_bootstrap',
     liveUrl: '/contribute-page',
-    sourseUrl: '/contribute-page',
-    img: 'images/works-images/gal-react.png',
-    name: 'bootstrap project',
-    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Ipsum has been the industry's standard dummy text
-    ever since the 1500s, when an unknown printer took a galley of
-    type and scrambled it 1960s with the releaLorem Ipsum is simply
-    dummy text of the printing and typesetting industry. Lorem Ipsum
-    has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it 1960s 
-    with the relea`,
-    technologies: ['javascript', 'ruby on rails', 'postgres'],
+    sourseUrl: 'https://github.com/ernestmusong/Annual-Income-Report',
+    img: 'images/works-images/income-statement.png',
+    name: 'Annual Income Report',
+    desc: `Pulls from the Financial modeling prep API to give annual report
+    of the income statements for the company "Apple Inc." for the past
+    thirty-eight years.`,
+    technologies: ['React', 'Redux', 'CSS'],
     skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
     skillsImg: 'images/icon-languages.png',
     desktopPopUpClose: 'images/desktop-popup-close.png',
@@ -65,19 +39,14 @@ const data = [
   {
     id: 4,
     detailUrl: '#_rails',
-    liveUrl: '/contribute-page',
-    sourseUrl: '/contribute-page',
-    img: 'images/works-images/gal-react.png',
-    name: 'ruby on rails',
-    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Ipsum has been the industry's standard dummy text
-    ever since the 1500s, when an unknown printer took a galley of
-    type and scrambled it 1960s with the releaLorem Ipsum is simply
-    dummy text of the printing and typesetting industry. Lorem Ipsum
-    has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it 1960s 
-    with the relea`,
-    technologies: ['javascript', 'ruby on rails', 'postgres'],
+    liveUrl: 'https://musong-bookstore.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/Bookstore',
+    img: 'images/works-images/bookstore.png',
+    name: 'Bookstore',
+    desc: `Pulls from the Bookstore api to display a
+    list of books, with the following features: Add,
+    Remove, and Edit.`,
+    technologies: ['React', 'Redux', 'CSS'],
     skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
     skillsImg: 'images/icon-languages.png',
     desktopPopUpClose: 'images/desktop-popup-close.png',
@@ -88,19 +57,15 @@ const data = [
   {
     id: 5,
     detailUrl: '#_rails',
-    liveUrl: '/contribute-page',
-    sourseUrl: '/contribute-page',
-    img: 'images/works-images/gal-react.png',
-    name: 'ruby on rails',
-    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Ipsum has been the industry's standard dummy text
-    ever since the 1500s, when an unknown printer took a galley of
-    type and scrambled it 1960s with the releaLorem Ipsum is simply
-    dummy text of the printing and typesetting industry. Lorem Ipsum
-    has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it 1960s 
-    with the relea`,
-    technologies: ['javascript', 'ruby on rails', 'postgres'],
+    liveUrl: 'https://musong-math-magicians.onrender.com/',
+    sourseUrl: 'https://github.com/ernestmusong/Math-magicians',
+    img: 'images/works-images/React-calculator.PNG',
+    name: 'math magicians',
+    desc: `A single page application containing three pages: a welcome
+    page, a calculator, and a quote page. The quote page pulls from the
+    Ninja API and displays a random quote. The calculator component 
+    contains a calculator that performs mathematical operations.`,
+    technologies: ['React', 'APIs', 'CSS'],
     skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
     skillsImg: 'images/icon-languages.png',
     desktopPopUpClose: 'images/desktop-popup-close.png',
@@ -111,19 +76,53 @@ const data = [
   {
     id: 6,
     detailUrl: '#_rails',
-    liveUrl: '/contribute-page',
-    sourseUrl: '/contribute-page',
-    img: 'images/works-images/gal-react.png',
-    name: 'ruby on rails',
-    desc: `Lorem Ipsum is simply dummy text of the printing and typesetting
-    industry. Lorem Ipsum has been the industry's standard dummy text
-    ever since the 1500s, when an unknown printer took a galley of
-    type and scrambled it 1960s with the releaLorem Ipsum is simply
-    dummy text of the printing and typesetting industry. Lorem Ipsum
-    has been the industry's standard dummy text ever since the 1500s,
-    when an unknown printer took a galley of type and scrambled it 1960s 
-    with the relea`,
-    technologies: ['javascript', 'ruby on rails', 'postgres'],
+    liveUrl: 'https://musong-react-pomodoro-clock.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/React-Pomodoro-clock',
+    img: 'images/works-images/pomo.png',
+    name: 'pomodoro clock',
+    desc: `Pomodoro clock is a timer clock built with React that has 
+    a break length of 5 minutes and a session length of 25 minutes by 
+    default. User can set the break and session lengths by clicking 
+    the (+) or (-) keys, play and pause the clock. The end of every 
+    session is indicated by a sound produced by the machine. `,
+    technologies: ['React', 'HTML', 'CSS'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    id: 7,
+    detailUrl: '#_rails',
+    liveUrl: 'https://ernest-markdown-previewer.netlify.app/',
+    sourseUrl: 'https://github.com/ernestmusong/React-Markdown-Previewer',
+    img: 'images/works-images/markdown.png',
+    name: 'markdown previewer',
+    desc: `Markdown previewer is a tool that allows you to write and preview 
+    Markdown-formatted text in real-time. It allows you to format text using 
+    simple syntax, such as adding asterisks around words to make them bold, 
+    or adding hash marks to create headings.`,
+    technologies: ['React', 'HTML', 'CSS'],
+    skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
+    skillsImg: 'images/icon-languages.png',
+    desktopPopUpClose: 'images/desktop-popup-close.png',
+    mobilePopUpClose: 'images/close-detail.png',
+    seeLiveIcon: 'images/see-live.png',
+    seeSourceIcon: 'images/see-source.png',
+  },
+  {
+    id: 8,
+    detailUrl: '#_rails',
+    liveUrl: 'https://becuda.netlify.app/#/',
+    sourseUrl: 'https://github.com/ernestmusong/Cultural-website',
+    img: 'images/works-images/becuda.png',
+    name: 'Cultural website',
+    desc: `Cultural website enables users to signup, login, contribute, keep
+    track of their contributions and see records of all contributions of my 
+    cultural association.`,
+    technologies: ['React', 'HTML', 'CSS'],
     skills: ['javascript', 'ruby on rails', 'postgres', 'html'],
     skillsImg: 'images/icon-languages.png',
     desktopPopUpClose: 'images/desktop-popup-close.png',
@@ -328,4 +327,24 @@ form.addEventListener('submit', (event) => {
     showError();
     event.preventDefault();
   }
+});
+
+// SCROLL TO SPECIFIC SPOT
+
+const navbar = document.getElementById("nav");
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    //navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navbar.getBoundingClientRect().height;
+    let position = element.offsetTop - navHeight;
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+  });
 });
